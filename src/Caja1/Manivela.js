176fde08a12005299e812e746c53c1265e5800be
Jose Jimenez Cazorla <josejc2001@gmail.com>
2023-05-23 22:06:52 +0200

    Manivela perfecta

--- src/Caja1/Manivela.js
+++ src/Caja1/Manivela.js
@@ -7,7 +7,7 @@ class Manivela extends THREE.Object3D{
         super();
         var objectLoader = new OBJLoader();
 
-        objectLoader.load('../../models/manivela/manivela.obj',
+        objectLoader.load('../../models/manivela/Manivela.obj',
         (object) => {
             this.modelo = object;
             this.add(this.modelo);
@@ -16,8 +16,13 @@ class Manivela extends THREE.Object3D{
     }
 
     posicionarHabitacion(){
+        //this.rotateZ(Math.PI/2);
+        this.rotateY(Math.PI/2);
         this.rotateZ(Math.PI/2);
-        this.scale.set(0.5,0.5,0.5);
+        this.scale.set(0.2,0.2,0.2);
+        this.position.x = -85;
+        this.position.y = 35;
+        this.position.z = -5;
     }
 
 }
